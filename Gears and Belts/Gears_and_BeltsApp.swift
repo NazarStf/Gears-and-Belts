2a3b7957d3965fb6e69296644782f94ae52b95c3
NazarStf <hworker98@gmail.com>
2023-07-29 18:23:59 +0300

    Initial Commit

---
+++ Gears and Belts/Gears_and_BeltsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gears_and_BeltsApp.swift
+//  Gears and Belts
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Gears_and_BeltsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
